e6810eb3fd31055b18ce12b8cad478d88b90ea7c
Luong Khoa <lnkhoa12052000@gmail.com>
2024-04-02 09:29:43 +0700

    <update> google conlose system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/provider/theme-provider";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout-components/header";
 import Banner from "@/components/layout-components/banner";
 import Menu from "@/components/layout-components/menu";
 import QueryProvider from "@/components/provider/query-provider";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 export const metadata: Metadata = {
   title: "Check Cost",
   description: "@copyright by Lương Khoa",
@@ -29,6 +29,11 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7465388386459244"
           crossOrigin="anonymous"
         ></script>
+        <meta
+          name="google-site-verification"
+          content="e9vIFzcxEFBGtl6BgwOBl0ESvqPLPLIgPI9oY7Ip6WY"
+        />
+         <Script src="https://www.google-analytics.com/analytics.js" />
       </head>
       <body className="relative h-screen flex flex-col">
         <QueryProvider>
